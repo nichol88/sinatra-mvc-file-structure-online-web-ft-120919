a2a0b3f2c42913b1160a7442ee87d81ca6481763
Brian Nicholls <nichol88@gmail.com>
2020-01-24 20:28:37 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,6 @@
 class Dog < Sinatra::Base
-  attr_accessor :name, :breed
-  def initialize(name, breed)
+  attr_accessor :name, :breed, :age
+  def initialize(name, breed, age)
     @name = name
     @breed = breed
   end
